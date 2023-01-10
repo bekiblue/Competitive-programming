8c02363136837ea48f587cf849bde1428ca22f56
Bereket Asnake Tsegaye <93071768+Beki4Git@users.noreply.github.com>
2023-01-10 23:53:11 +0300

    Updating the solution  for better complexity by the concept of monotonic stack

--- daily_temperatures.py
+++ daily_temperatures.py
@@ -1,13 +1,10 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        answer=[0]*len(temperatures)
-
-        for i in range(len(temperatures)-1):
-            for j in range(i+1,len(temperatures)):
-                if temperatures[i]<temperatures[j]:
-                    answer[i]=j-i
-                    break
+        answer = [0] * len(temperatures)
+        stack = []
+        for i, t in enumerate(temperatures):
+            while stack and t > temperatures[stack[-1]]:
+                j = stack.pop()
+                answer[j] = i - j
+            stack.append(i)
         return answer
-            
-
-        
