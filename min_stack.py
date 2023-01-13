1813cc4af41e009edf1defd0a15b884a5deb8aa8
Bereket Asnake Tsegaye <93071768+Beki4Git@users.noreply.github.com>
2023-01-13 20:30:07 +0300

    Improving the time complexity using the concept of monotonic stack

--- min_stack.py
+++ min_stack.py
@@ -3,18 +3,26 @@ class MinStack:
     def __init__(self):
         
         self.stack=[]
+        self.min=[]
 
     def push(self, val: int) -> None:
         self.stack.append(val)
+        if self.min and val<=self.min[-1]:
+            self.min.append(val)
+        elif self.min and val>self.min[-1]:
+            self.min.append(self.min[-1])
+        else : 
+            self.min.append(val)
 
     def pop(self) -> None:
         self.stack.pop()
+        self.min.pop()
         
     def top(self) -> int:
         return self.stack[-1]
-        
+
     def getMin(self) -> int:
-        return min(self.stack)
+      return self.min[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
