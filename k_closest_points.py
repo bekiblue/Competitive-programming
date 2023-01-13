c46b6096572d114fc59c6b45ec427c7ea9c2c6da
Bereket Asnake Tsegaye <93071768+Beki4Git@users.noreply.github.com>
2023-01-13 23:45:03 +0300

    Improving the complexity using sort method

--- k_closest_points.py
+++ k_closest_points.py
@@ -1,14 +1,4 @@
 class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        distance=[]
-        for point in points:
-            distance.append(point[0]*point[0]+point[1]*point[1])
-        
-        for i in range(len(distance)):
-            min=i
-            for j in range(i+1,len(distance)):
-                if distance[j]<distance[min]:
-                    min=j
-            distance[i],distance[min]=distance[min],distance[i]  
-            points[i],points[min]=points[min],points[i]  
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]: 
+        points.sort(key=lambda x: x[0]*x[0]+x[1]*x[1])
         return points[0:k]
