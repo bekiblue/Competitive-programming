859441241966a826ae477777bcaca8c432ef3bc7
Bereket Asnake Tsegaye <93071768+Beki4Git@users.noreply.github.com>
2023-01-20 21:59:16 +0300

    The Solution of reverse linked list problem on LeetCode

---
+++ reverse_linked_list.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev,cur=None,head
+        while cur:
+            temp=cur.next
+            cur.next=prev
+            prev=cur
+            cur=temp 
+        head=prev
+
+        return head
