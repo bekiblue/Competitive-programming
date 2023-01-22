26ec06f1672877f759103f6adf15110ecd5361d5
Bereket Asnake Tsegaye <93071768+Beki4Git@users.noreply.github.com>
2023-01-22 11:18:55 +0300

    Updating Add Two Numbers Problem on LeetCode

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -5,17 +5,17 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:     
-        anchor = 0
-        preHead = ListNode(0)
-        current = preHead
-        while not (l1 == None and l2 == None and anchor == 0):
+        dummy=ListNode()
+        cur=dummy
+        anchor=0
+        while l1 or l2 or anchor:
             val1 = l1.val if l1 else 0
             val2 = l2.val if l2 else 0
             sum = val1 + val2 + anchor
             anchor = sum // 10
             node = ListNode(sum % 10)
-            current.next = node
-            current = node
+            cur.next = node
+            cur = node
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-        return preHead.next
+        return dummy.next
