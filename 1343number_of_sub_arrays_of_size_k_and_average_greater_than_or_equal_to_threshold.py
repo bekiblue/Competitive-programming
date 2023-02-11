9590a778ff06f972a7798bbfcfc7bff6aed1f70a
Bereket Asnake Tsegaye <93071768+Beki4Git@users.noreply.github.com>
2023-02-11 23:12:40 +0300

    The Solution for Number of Sub-arrays of Size K and Average Greater than or Equal to problem on LeetCode

--- 1343number_of_sub_arrays_of_size_k_and_average_greater_than_or_equal_to_threshold.py
+++ 1343number_of_sub_arrays_of_size_k_and_average_greater_than_or_equal_to_threshold.py
@@ -1,1 +1,16 @@
 
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        answer = 0
+        l = 0
+        r = k-1
+        cur_sum = sum(arr[0:k-1])
+        while r < len(arr):
+            cur_sum += arr[r]
+            if cur_sum / k >= threshold:
+                answer += 1
+            cur_sum -= arr[l]
+            l += 1
+            r += 1
+        
+        return answer
