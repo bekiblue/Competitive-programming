dee284540aadc6f96d9d13dd09224591898c1b21
Bereket Asnake Tsegaye <93071768+bekiblue@users.noreply.github.com>
2023-12-12 16:33:24 +0300

    Update E_Pivot_Sorting.py

--- contests/week2/E_Pivot_Sorting.py
+++ contests/week2/E_Pivot_Sorting.py
@@ -1,15 +1,16 @@
 t = int(input())
 for _ in range(t):
-  left,right = 0,10**9
+  mnm,mxm = 0,10**9
   n = int(input())
   a = list(map(int,input().split()))
   for index in range(n-1):
-    x,y = a[index],a[index+1]
-    if x > y:
-      left = max(left,(x+y+1)//2)
-    elif x < y:
-      right = min(right,(x+y)//2)
-  if left <= right:
-    print(left)
+    cur,next = a[index],a[index+1]
+    if cur > next:
+      mnm = max(mnm,(cur+next+1)//2)
+    elif cur < next:
+      mxm = min(mxm,(cur+next)//2)
+  if mnm <= mxm:
+    print(mnm)
   else:
     print(-1)
+ 
