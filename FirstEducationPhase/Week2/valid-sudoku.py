6040c5e6dbffed5765df53d706839f62d0b5422d
Bereket Asnake Tsegaye <93071768+bekiblue@users.noreply.github.com>
2023-12-26 22:54:55 +0300

    Added solution for Valid Sudoku

--- FirstEducationPhase/Week2/valid-sudoku.py
+++ FirstEducationPhase/Week2/valid-sudoku.py
@@ -2,8 +2,9 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         row=defaultdict(set)
         col=defaultdict(set)
-        for r in range(len(board)):
-            for c in range(len(board[0])):
+        cell=defaultdict(set)
+        for r in range(9):
+            for c in range(9):
                 if board[r][c]==".":
                     continue
                 if board[r][c] in row[r]:
@@ -12,15 +13,9 @@ class Solution:
                 if board[r][c] in col[c]:
                     return False 
                 col[c].add(board[r][c])
-        for row in range(0,len(board),3):
-            for col in range(0,len(board[0]),3):
-                cell=set()
-                for r in range(row,row+3):
-                    for c in range(col,col+3):
-                        if board[r][c]==".":
-                            continue
-                        elif board[r][c] in cell:
-                            return False
-                        cell.add(board[r][c])
+                if board[r][c] in cell[(r//3,c//3)]:
+                    return False
+                cell[(r//3,c//3)].add(board[r][c])
+        
         return True
 
