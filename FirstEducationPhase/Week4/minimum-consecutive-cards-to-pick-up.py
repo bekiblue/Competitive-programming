b52b948d5e3911c466df447ae0863cf708de3b5e
Bereket Asnake Tsegaye <93071768+bekiblue@users.noreply.github.com>
2024-01-11 18:07:36 +0300

    Added solution for Minimum Consecutive Cards to Pick Up

--- FirstEducationPhase/Week4/minimum-consecutive-cards-to-pick-up.py
+++ FirstEducationPhase/Week4/minimum-consecutive-cards-to-pick-up.py
@@ -1,12 +1,13 @@
 class Solution:
     def minimumCardPickup(self, cards: List[int]) -> int:
+        mnm=float("inf")
         index={}
         left=0
-        mnm=len(cards)+1
         for right in range(len(cards)):
-            if cards[right] in index:
+            if cards[right] in index and index[cards[right]] >= left:
                 left=index[cards[right]]
                 mnm=min(mnm,right-left+1)
             index[cards[right]]=right
-        return mnm if mnm<len(cards)+1 else -1
-             
+        return mnm if mnm<float("inf") else -1
+            
+        
