85db7ef55867207833283a36876d88c99b6a3669
Bereket Asnake Tsegaye <93071768+bekiblue@users.noreply.github.com>
2024-01-12 21:53:12 +0300

    Added solution for Count Number of Nice Subarrays

--- FirstEducationPhase/Week4/count-number-of-nice-subarrays.py
+++ FirstEducationPhase/Week4/count-number-of-nice-subarrays.py
@@ -1,20 +1,23 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        next_odd_index=len(nums)
-        next_odd={}
-        for i in range(len(nums)-1,-1,-1):
-            next_odd[i]=next_odd_index
-            if nums[i]%2 != 0:
-                next_odd_index=i
+        # for each index count the nice subarrays that end at that index
+        total=0
         odds=0
         nice=0
         left=0
         for right in range(len(nums)):
             if nums[right]%2 != 0:
                 odds+=1
+                # if an odd number odd is encountered my nice subarray will become bad 
+                nice=0 
             while odds==k:
-                nice+=(next_odd[right]-right)
-                if nums[left]%2!=0:
+                #count the number all nice subarrays that end at this index
+                nice+=1
+                if nums[left]%2!= 0:
                     odds-=1
                 left+=1
-        return nice
+            #add the number of nice subarrays at this index to the total
+            total+=nice
+        return total
+
+
