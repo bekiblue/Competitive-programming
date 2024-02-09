513e432a1c1e802508fe3e478a5997e0b1961fea
Bereket Asnake Tsegaye <93071768+bekiblue@users.noreply.github.com>
2024-02-09 11:06:26 +0300

    Add solution for Sum of Distances

--- Camp 1/week1/leetcode/sum-of-distances.py
+++ Camp 1/week1/leetcode/sum-of-distances.py
@@ -1,24 +1,23 @@
 class Solution:
     def distance(self, nums: List[int]) -> List[int]:
         prefix=[0]*len(nums)
-        p_p={}
-        for idx in range(len(nums)):
-            if nums[idx] in p_p:
-                prefix[idx]=prefix[p_p[nums[idx]][0]]+(p_p[nums[idx]][1])*(idx-p_p[nums[idx]][0])
-                p_p[nums[idx]][0]=idx
-                p_p[nums[idx]][1]+=1
+        previous_prefix={}
+        for index in range(len(nums)):
+            if nums[index] in previous_prefix:
+                prefix[index]=prefix[previous_prefix[nums[index]][0]]+(previous_prefix[nums[index]][1])*(index-previous_prefix[nums[index]][0])
+                previous_prefix[nums[index]][0]=index
+                previous_prefix[nums[index]][1]+=1
             else:
-                p_p[nums[idx]]=[idx,1]
-        print(prefix)
+                previous_prefix[nums[index]]=[index,1]
         suffix=[0]*len(nums)
-        p_s={}
-        for idx in range(len(nums)-1,-1,-1):
-            if nums[idx] in p_s:
-                suffix[idx]=suffix[p_s[nums[idx]][0]]+(p_s[nums[idx]][1])*(p_s[nums[idx]][0]-idx)
-                p_s[nums[idx]][0]=idx
-                p_s[nums[idx]][1]+=1
+        previous_suffix={}
+        for index in range(len(nums)-1,-1,-1):
+            if nums[index] in previous_suffix:
+                suffix[index]=suffix[previous_suffix[nums[index]][0]]+(previous_suffix[nums[index]][1])*(previous_suffix[nums[index]][0]-index)
+                previous_suffix[nums[index]][0]=index
+                previous_suffix[nums[index]][1]+=1
             else:
-                p_s[nums[idx]]=[idx,1]
+                previous_suffix[nums[index]]=[index,1]
         arr=[]
         for i in range(len(nums)):
             arr.append(prefix[i]+suffix[i])
