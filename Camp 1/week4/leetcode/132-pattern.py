a80b56d6aee64ba427f931678f9f5bfaf666a595
Bereket Asnake Tsegaye <93071768+bekiblue@users.noreply.github.com>
2024-02-26 22:35:36 +0300

    Add solution for 132 Pattern

--- Camp 1/week4/leetcode/132-pattern.py
+++ Camp 1/week4/leetcode/132-pattern.py
@@ -1,21 +1,21 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        mon_incr=[]
-        smallest_smaller=[]
+        mono_decr=[]
+        smallest=[]
+        cur_smallest=0
         for index,num in enumerate(nums):
-            while mon_incr and num < nums[mon_incr[-1]]:
-                mon_incr.pop()
-            if mon_incr:
-                smallest_smaller.append(nums[mon_incr[0]])
-            else:
-                smallest_smaller.append(float("inf"))
-            mon_incr.append(index) 
-        mon_decr=[]
-        for index in range(len(nums)-1,-1,-1):
-            i=None
-            while mon_decr and nums[index] > nums[mon_decr[-1]]:
-                i=mon_decr.pop()
-            if i is not None and smallest_smaller[index] < nums[i]:
-                return True
-            mon_decr.append(index)
+            # Handles for keeping the smallest element index upto current index
+            if num < nums[cur_smallest]:
+                cur_smallest=index
+            smallest.append(cur_smallest)
+
+            #updates the stack  
+            while mono_decr and num >= nums[mono_decr[-1]]:
+                mono_decr.pop()
+
+            #return True if there is a previous greater element and the smallest element upto that greater element index is less than the current element 
+            if mono_decr and nums[smallest[mono_decr[-1]]] < num:
+                    return True
+            mono_decr.append(index) 
+
         return False
