3eb5422f7bc3908ccdd79483d77fd1fb25005350
Bereket Asnake Tsegaye <93071768+bekiblue@users.noreply.github.com>
2024-03-16 07:11:37 +0300

    Add solution for Find First and Last Position of Element in Sorted Array

--- SecondEducationPhase/Week1/leetcode/find-first-and-last-position-of-element-in-sorted-array.py
+++ SecondEducationPhase/Week1/leetcode/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,37 +1,26 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        # Find the index of the last number less than the target
-        start = -1
+        # Find the index of the last number less than the target 
+        left = 0
+        right = len(nums) - 1
+        while left <= right :
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1 
+            else:
+                right = mid - 1
+        start = left if (left < len(nums) and nums[left] == target) else -1
+        # Find the index of the first number greater than the target
         left = 0
         right = len(nums) - 1
 
         while left <= right :
             mid = (left + right) // 2
-            if nums[mid] == target :
-                right = mid
-                start = right  
-                if left == right :
-                    break               
-            elif nums[mid] < target:
-                left = mid + 1 
-            else:
-                right = mid - 1
-        # Find the index of the first number greater than the target
-        left = 0
-        right = len(nums) - 1
-        end = -1
-        while left <= right :
-            mid = (left + right) // 2
-            if nums[mid] == target :
-                left = mid
-                end = left
-                if left == right or right == left + 1 :
-                    if  nums[left] == nums[right] :
-                        end = right
-                    break
-            elif nums[mid] < target:
+
+            if nums[mid] <= target:
                 left = mid + 1
             else:
                 right = mid - 1 
-                
+        end = right if (right >= 0 and nums[right] == target) else -1
+        
         return [start , end]
